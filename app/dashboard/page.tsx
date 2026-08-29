99f41bbbdf169a3152887e68d44c3f1b6888325e
reminiscense <dima0911v@gmail.com>
2026-08-30 01:21:27 +0300

    feat: show login state on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,34 @@
-
+import { auth } from "../auth"
+import { signInWithGoogle } from "@/actions";
 
 export default async function Dashboard() {
+    const session = await auth()
     return (
         <div>
-            Test
+            {session?.user ? (
+                <>
+                    <p>Hello, {session.user.name}</p>
+                    {<form action={signInWithGoogle}>
+                        <button
+                            type="submit"
+                            className="px-4 py-2 bg-black text-white dark:bg-white dark:text-black rounded-md"
+                        >
+                            Sign in with Google
+                        </button>
+                    </form>}
+                </>
+            ) : (
+                <>
+                    {<form action={signInWithGoogle}>
+                        <button
+                            type="submit"
+                            className="px-4 py-2 bg-black text-white dark:bg-white dark:text-black rounded-md"
+                        >
+                            Sign in with Google
+                        </button>
+                    </form>}
+                </>
+            )}
         </div>
     )
 }
